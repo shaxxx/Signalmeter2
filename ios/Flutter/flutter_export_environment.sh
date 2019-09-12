3910124cfa5989a8818ad9a897c7a7dddd756942
shax <shax@Mojave.local>
2019-09-12 11:04:24 +0200

    ios project updates

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/shax/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/shax/Documents/repos/Signalmeter2"
-export "FLUTTER_TARGET=/Users/shax/Documents/repos/Signalmeter2/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/shax/Documents/flutter/bin/cache/artifacts/engine/ios-release"
